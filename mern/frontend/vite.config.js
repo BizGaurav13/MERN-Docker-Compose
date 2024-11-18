d0edde58423b9e42134be9abec313714effd31bf
BizGaurav13 <gaurav.ks@mail.biztechcs.com>
2024-11-18 18:55:41 +0530

    feat: Added hot reloading with docker in dev mode

--- mern/frontend/vite.config.js
+++ mern/frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // Allow external connections (e.g., Docker)
+    port: 5173,
+    strictPort: true, // Ensures no port conflicts
+    watch: {
+      usePolling: true, // Enables polling for file changes
+    },
+  },
+});
